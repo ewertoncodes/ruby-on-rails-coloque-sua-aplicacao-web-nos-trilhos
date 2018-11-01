7169a3aaf94a6dbd3ac4517f10d3ef9ac6b92594
ewertonorg <ewertonorg@gmail.com>
2018-11-01 04:32:44 -0300

    Capítulo 4 Implementação do modelo para o cadastro de usuários

---
+++ colchonet/app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ActiveRecord::Base
+  EMAIL_REGEXP = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
+
+  # validates_presence_of :email, :full_name, :location, :password
+  # validates_confirmation_of :password
+  # validates_length_of :bio, minimum: 30, allow_blank: false
+  validates_uniqueness_of :email
+  #validate :email_format
+  
+  private
+  
+  validate do
+    errors.add(:email, :invalid) unless email.match(EMAIL_REGEXP)
+  end
+end
--- colchonet/db/schema.rb
+++ colchonet/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181101065345) do
+ActiveRecord::Schema.define(version: 20181101071046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 20181101065345) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "full_name"
+    t.string   "email"
+    t.string   "password"
+    t.string   "location"
+    t.text     "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
